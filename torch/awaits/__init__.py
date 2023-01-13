5490ce19d7a6b56080735331104d568e0b3720e2
Ivan Kobzarev <ivankobzarev@fb.com>
2023-01-13 08:04:24 -0800

    [jit] Support Await type  in jit
    ghstack-source-id: a251ee40e829b3369aa9ec5045d19ed1f8da5356
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/90863

---
+++ torch/awaits/__init__.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+from typing import cast, Callable, Generic, Type, TypeVar
+
+import torch
+
+__all__ = ['Await']
+
+W = TypeVar("W")
+
+class _PyAwaitMeta(type(torch._C.Await), type(Generic)):  # type: ignore[misc, no-redef]
+    pass
+
+class Await(torch._C.Await, Generic[W], metaclass=_PyAwaitMeta):
+    pass
